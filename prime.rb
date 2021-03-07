c6ff6bc8042c01b148b9ed62461fdcce55b5c11f
Clayon Stewart <c.stewart89@gmail.com>
2021-03-07 20:55:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 # Add  code here!
 def prime?(number)
   number = number.abs
-  puts number
   if number <= 1
-    return true
+    return false
   else
     (2..(number - 1)).each do |n|
       if number % n == 0
