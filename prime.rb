a0a26b0e585eb0b8b354edcc21004a554bc14355
Clayon Stewart <c.stewart89@gmail.com>
2021-03-07 20:26:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,14 @@
 # Add  code here!
 def prime?(number)
-  (2..(number - 1)).each do |n|
-    return false if number % n == 0
+  if number > 0
+    (2..(number - 1)).each do |n|
+      return false if number % n == 0
+    end
+    true
+  elsif number < 0
+    (-2..(number + 1)).each do |n|
+      return false if number % n == 0
+    end
+    true
   end
-  true
 end
